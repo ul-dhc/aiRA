a7092e98c258bf1ffe80951017c6e99b00fe0663
uldhc <dhc@lu.lv>
2026-08-04 15:22:42 +0300

    Add files via upload

---
+++ aiRA-multilingual/src/app/components/translate-description-dialog/translate-description-dialog.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose,
+         MatDialogContent, MatDialogTitle
+        } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+
+import { ImageData } from '../../types/image-data.types';
+import { SettingsService } from '../../services/settings.service';
+import { LocaleService } from '../../i18n/locale.service';
+import { LanguageCode } from '../../../assets/config/prompts';
+
+@Component({
+  selector: 'translate-description-dialog',
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    MatRadioModule
+  ],
+  templateUrl: './translate-description-dialog.component.html',
+  styleUrl: './translate-description-dialog.component.scss'
+})
+export class TranslateDescriptionDialogComponent implements OnInit {
+  imageObj = inject<ImageData>(MAT_DIALOG_DATA);
+  readonly settings = inject(SettingsService);
+  readonly locale = inject(LocaleService);
+
+  translateLanguages: any[] = [];
+  selectedLanguageCode?: LanguageCode;
+
+  ngOnInit(): void {
+    const currentLanguage = this.imageObj.descriptions[this.imageObj.activeDescriptionIndex].language;
+
+    // Filter out the source language
+    this.settings.languages().forEach((lang: any) => {
+      if (lang.code !== currentLanguage) {
+        this.translateLanguages.push({
+          languageCode: lang.code,
+          languageDisplayName: lang.name
+        });
+
+        // Set the default selected language to the first language in the list
+        if (!this.selectedLanguageCode) {
+          this.selectedLanguageCode = lang.code;
+        }
+      }
+    });
+  }
+}
